85a9d983e36a97b3d7f0f45309de9ad461dc91ec
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-05-10 17:05:04 +0300

    Stylize DeveloperView, render additional info

--- frontend/src/components/DeveloperView/index.tsx
+++ frontend/src/components/DeveloperView/index.tsx
@@ -1,43 +1,62 @@
 import React from 'react';
 
-import { Platform, View } from 'react-native';
+import { ActivityIndicator, Platform, View } from 'react-native';
+import { useTheme } from 'react-native-paper';
 
 import StyledText from '../StyledText';
 
+import { AppTheme } from '../../types';
 import config from '../../utils/config';
 import useGosagoraService from '../../hooks/useGosagoraService';
 
 const ServiceStatus = () => {
+  const theme = useTheme<AppTheme>();
   const { status, loading, error } = useGosagoraService();
 
   if (loading) {
     return (
-      <View>
-        <StyledText>Service Status: loading..</StyledText>
+      <View style={{ flexDirection: "row" }}>
+        <StyledText variant="small">Service Status: </StyledText>
+        <ActivityIndicator size={theme.fonts.bodySmall.fontSize} />
       </View>
     );
   }
   if (error) {
+    console.log('Service status error:', error);
     return (
       <View>
-        <StyledText>Error getting Service Status: {error}</StyledText>
+        <StyledText variant="small">Service Status: Error</StyledText>
       </View>
     );
   }
 
   return (
     <View>
-      <StyledText>Service Status: {status}</StyledText>
+      <StyledText variant="small">Service Status: {status}</StyledText>
     </View>
   );
 };
 
 const DebugView = () => {
+  if (!config.IS_PRODUCTION_ENV && !config.FIREBASE_AUTH_EMULATOR_HOST) {
+    throw new Error('Invalid configuration. App is running outside production env and no firebase emulator connection configured');
+  }
+  const theme = useTheme<AppTheme>();
+
+  const getEnvStr = () => {
+    if (config.IS_PRODUCTION_ENV) return 'production';
+    if (config.IS_DEVELOPMENT_ENV) return 'development';
+    if (config.IS_TEST_ENV) return 'test';
+    throw new Error('Environment not set');
+  };
+
   return (
-    <View>
+    <View style={theme.styles.developerViewContainer}>
       <ServiceStatus />
-      <StyledText>REST backend URL: {config.BACKEND_BASE_URL}.</StyledText>
-      <StyledText>Platform: {Platform.OS}{Platform.OS !== 'web' && <>&nbsp;ver. {Platform.Version}</>}.</StyledText>
+      <StyledText variant="small">REST URI: {config.BACKEND_BASE_URL}</StyledText>
+      <StyledText variant="small">FB emultr: {config.FIREBASE_AUTH_EMULATOR_HOST}</StyledText>
+      <StyledText variant="small">Platform: {Platform.OS}{Platform.OS !== 'web' && <>&nbsp;ver. {Platform.Version}</>}.</StyledText>
+      <StyledText variant="small">Env: {getEnvStr()}</StyledText>
     </View>
   );
 };
