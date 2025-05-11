9c1fe0ff66301a105ed739ffae020e4d6e0ce136
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-05-11 11:29:11 +0300

    Relax linting rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -68,9 +68,6 @@ export default tseslint.config(
       ],
       'react-native/no-unused-styles': 2,
       'react-native/split-platform-components': 2,
-      'react-native/no-inline-styles': 2,
-      'react-native/no-color-literals': 2,
-      'react-native/no-raw-text': 2,
       'react-native/sort-styles': ['warn', 'asc', {
         'ignoreClassNames': false,
         'ignoreStyleProperties': false
