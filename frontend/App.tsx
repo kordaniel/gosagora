4575397b6dbec79a240df86f62e6dfcda4bc2554
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-05-13 14:59:17 +0300

    Frontend: Add react-navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 
 import { PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStaticNavigation } from '@react-navigation/native';
 
 import Main from './src/components/Main';
 
 import useAppTheme from './src/hooks/useAppTheme';
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: Main,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 const App = () => {
   const theme = useAppTheme();
   return (
     <SafeAreaProvider style={{ backgroundColor: theme.colors.background }}>
       <PaperProvider theme={theme}>
-        <Main />
+        <Navigation />
       </PaperProvider>
     </SafeAreaProvider>
   );
