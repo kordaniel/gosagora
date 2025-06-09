ba625a833f03cf34d5ab3ec56b09a99e52f45d2d
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-06-09 15:55:19 +0300

    Frontend: configure expo metro bundler to include common package

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,8 +1,20 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
+
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '..');
+
 /** @type {import('expo/metro-config').MetroConfig} */
-const defaultConfig = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(projectRoot);
 defaultConfig.resolver.sourceExts.push('cjs');
+
+defaultConfig.resolver.extraNodeModules = {
+  '@common': path.resolve(workspaceRoot, 'packages/common'),
+};
+defaultConfig.watchFolders = [
+  path.resolve(workspaceRoot, 'packages/common'),
+];
 
 module.exports = defaultConfig;
