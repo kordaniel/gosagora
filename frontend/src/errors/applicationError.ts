8b8033fd7d77791ffebc95eaa78669aed29760bb
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-06-18 16:08:17 +0300

    Frontend: Add AuthError ApplicationError

--- frontend/src/errors/applicationError.ts
+++ frontend/src/errors/applicationError.ts
@@ -1,6 +1,6 @@
 type ApplicationErrorKind =
-  | 'HttpError'
-  | 'TemporaryUnionFillerError';
+  | 'AuthError'
+  | 'HttpError';
 
 export abstract class ApplicationError extends Error {
   abstract readonly kind: ApplicationErrorKind;
@@ -11,30 +11,27 @@ export abstract class ApplicationError extends Error {
   }
 }
 
+export class AuthError extends ApplicationError {
+  kind = 'AuthError' as const;
+  constructor(
+    public override message: string = 'Please Sign In to perform this action',
+  ) {
+    super();
+    Object.setPrototypeOf(this, AuthError.prototype);
+  }
+}
+
 export class HttpError extends ApplicationError {
   kind = 'HttpError' as const;
   constructor(
-    public override message: string
+    public override message: string,
   ) {
     super();
     Object.setPrototypeOf(this, HttpError.prototype);
   }
 }
 
-// TODO: Delete this temporary class that should never be used. It's defined
-//       here only so we can use CorsError in a discriminated union before
-//       adding additional Errors.
-export class TemporaryUnionFillerError extends ApplicationError {
-  kind = 'TemporaryUnionFillerError' as const;
-  constructor(
-    public override message: string = 'Internal server (developer) error. Should never be thrown',
-  ) {
-    super();
-    Object.setPrototypeOf(this, TemporaryUnionFillerError.prototype);
-  }
-}
-
 export type ApplicationErrorType =
   | ApplicationError
-  | HttpError
-  | TemporaryUnionFillerError;
+  | AuthError
+  | HttpError;
