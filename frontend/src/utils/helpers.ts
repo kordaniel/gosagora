d9080f6331772c3f2304b3a8448202738e628efe
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-07-23 12:30:57 +0300

    Frontend: Add clampString

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -4,6 +4,14 @@ export const createReverseEnumMap = <T extends Record<string, string>>(enumObj: 
   ) as { [K in T[keyof T]]: keyof T };
 };
 
+export const clampString = (str: string, len: number): string => {
+  if (str.length <= len) {
+    return str;
+  }
+
+  return `${str.substring(0, len-3)}...`;
+};
+
 /**
  * Pause execution for ms amount of time before continuing. This function is
  * primarily intended to be used during development, for mimicing latency.
