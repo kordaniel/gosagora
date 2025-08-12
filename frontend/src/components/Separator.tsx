c5ae088e5e0951b8ee94eacc234e73914527733a
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-08-12 11:21:06 +0300

    Frontend: Add height prop

--- frontend/src/components/Separator.tsx
+++ frontend/src/components/Separator.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
 import { AppTheme } from '../types';
 
-const Separator = () => {
+interface SeparatorProps {
+  height?: number;
+}
+
+const Separator = ({ height }: SeparatorProps) => {
   const theme = useTheme<AppTheme>();
+  const style = StyleSheet.compose(
+    theme.styles.separator,
+    height === undefined ? undefined : { height }
+  );
+
   return (
-    <SafeAreaView mode="margin" style={theme.styles.separator} />
+    <SafeAreaView mode="margin" style={style} />
   );
 };
 
