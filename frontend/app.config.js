41fe3707991e2405105b282c282058911078c462
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-08-29 13:47:30 +0300

    Frontend: Add expo-location, expo-task-manager

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -2,25 +2,48 @@ import 'dotenv/config';
 
 export default {
   name: 'GosaGora',
-  slug: 'GosaGora',
+  slug: 'gosagora',
   version: '0.0.1',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'automatic', // https://docs.expo.dev/develop/user-interface/color-themes/
   newArchEnabled: true,
+  plugins: [[
+    'expo-location',
+    {
+      locationAlwaysAndWhenInUsePermission: 'Allow location access so we can provide you insights about your tracks and performance and to be able to deliver reliable race results.',
+      locationWhenInUsePermission: 'Allow location access so we can provide you insights about your tracks and performance and to be able to deliver reliable race results.',
+      isAndroidBackgroundLocationEnabled: true,
+    }
+  ]],
   splash: {
     image: './assets/splash-icon.png',
     resizeMode: 'contain',
     backgroundColor: '#ffffff',
   },
   ios: {
+    bundleIdentifier: 'com.kordaniel.gosagora',
     supportsTablet: true,
+    isTabletOnly: false,
+    infoPlist: {
+      UIBackgroundModes: ['location'],
+      NSLocationWhenInUseUsageDescription: 'Allow location access so we can provide you insights about your tracks and performance and to be able to deliver reliable race results.',
+      NSLocationAlwaysUsageDescription: 'Allow location access so we can provide you insights about your tracks and performance and to be able to deliver reliable race results.',
+      NSLocationAlwaysAndWhenInUseUsageDescription: 'Allow location access so we can provide you insights about your tracks and performance and to be able to deliver reliable race results.'
+    }
   },
   android: {
     adaptiveIcon: {
       foregroundImage: './assets/adaptive-icon.png',
       backgroundColor: '#ffffff',
-    }
+    },
+    package: 'com.kordaniel.gosagora',
+    permissions: [
+      'ACCESS_FINE_LOCATION',
+      'ACCESS_COARSE_LOCATION',
+      'ACCESS_BACKGROUND_LOCATION',
+      'FOREGROUND_SERVICE'
+    ]
   },
   web: {
     favicon: './assets/favicon.png',
