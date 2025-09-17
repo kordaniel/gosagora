0f9d92fa3c9453727010404df1168bf31abe1b89
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-09-17 15:06:02 +0300

    Frontend: Add math helper functions

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -4,6 +4,13 @@ export const createReverseEnumMap = <T extends Record<string, string>>(enumObj: 
   ) as { [K in T[keyof T]]: keyof T };
 };
 
+export const clampNumber = (value: number, min: number, max: number): number => {
+  if (min > max) {
+    throw new RangeError('clampNumber: argument min cannot be greater than argument max');
+  }
+  return Math.min(Math.max(value, min), max);
+};
+
 export const clampString = (str: string, len: number): string => {
   if (str.length <= len) {
     return str;
@@ -12,13 +19,21 @@ export const clampString = (str: string, len: number): string => {
   return `${str.substring(0, len-3)}...`;
 };
 
+export const randRange = (minInclusive: number, maxExclusive: number): number => {
+  return minInclusive + Math.random() * (maxExclusive-minInclusive);
+};
+
+export const randUnitRangeFrom = (minInclusive: number): number => {
+  return minInclusive + Math.random();
+};
+
 export const roundNumber = (value: number, decimals: number = 0): number => {
   const multiplier = Math.pow(10, decimals);
   return Math.round(value * multiplier) / multiplier;
 };
 
-export const truncateNumber = (val: number): number => {
-  return val > 0 ? Math.floor(val) : Math.ceil(val);
+export const truncateNumber = (value: number): number => {
+  return value > 0 ? Math.floor(value) : Math.ceil(value);
 };
 
 /**
