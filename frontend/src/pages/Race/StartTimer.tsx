0da4ab8906143b2a7adbc129b3a975b68b351ab2
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-09-29 16:35:52 +0300

    Frontend: Stylize StartTimer

--- frontend/src/pages/Race/StartTimer.tsx
+++ frontend/src/pages/Race/StartTimer.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
 
-import { ScrollView } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
+import { useTheme } from 'react-native-paper';
 
 import Button from '../../components/Button';
-import StyledText from '../../components/StyledText';
 
+import type { AppTheme } from '../../types';
 import { timeDurationToString } from '../../utils/stringTools';
 import useStartTimer from '../../hooks/useStartTimer';
 
 const StartTimer = () => {
-  const startTimerState = useStartTimer();
+  const theme = useTheme<AppTheme>();
+  const startTimer = useStartTimer();
+
   return (
-    <ScrollView>
-      <StyledText>{timeDurationToString(startTimerState.timeLeft)}</StyledText>
-      <Button disabled={true}><StyledText variant="button">-1 Min</StyledText></Button>
-      <Button disabled={true}><StyledText variant="button">+1 Min</StyledText></Button>
-      <Button disabled={startTimerState.isCounting} onPress={startTimerState.reset}><StyledText variant="button">Reset</StyledText></Button>
-      <Button disabled={true}><StyledText variant="button">Sync</StyledText></Button>
-      {startTimerState.isCounting
-        ? <Button onPress={startTimerState.pause}><StyledText variant="button">Pause</StyledText></Button>
-        : <Button onPress={startTimerState.start}><StyledText variant="button">Start</StyledText></Button>
-      }
+    <ScrollView contentContainerStyle={{ flexDirection: 'column' }}>
+      <View>
+        <Text>{timeDurationToString(startTimer.timeLeft)}</Text>
+      </View>
+
+      <View style={{ flexDirection: 'column' }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <Button style={{ flexGrow: 1 }} disabled={true}>-1 Min</Button>
+          <Button style={{ flexGrow: 1 }} disabled={true}>+1 Min</Button>
+        </View>
+        <Button
+          disabled={startTimer.isCounting}
+          onPress={startTimer.reset}>
+            Reset
+        </Button>
+        <Button disabled={true}>Sync</Button>
+        {startTimer.isCounting
+          ? <Button
+              colors={[theme.colors.error, 'white']}
+              ctxLoading={startTimer.isCounting}
+              onPress={startTimer.pause}
+            >
+              Pause
+            </Button>
+          : <Button
+              colors={[theme.colors.tertiary, 'white']}
+              onPress={startTimer.start}
+            >
+              Start
+            </Button>
+        }
+      </View>
     </ScrollView>
   );
 };
