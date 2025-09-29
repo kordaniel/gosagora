ab7a4d28dbcf49ba41f37ce223ea50e23b8e544e
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-09-29 17:30:35 +0300

    Frontend: Extend AppTheme with customFonts

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,6 +1,7 @@
 import type { ImageStyle, TextStyle, ViewStyle } from 'react-native';
 import type { Route, SceneRendererProps } from 'react-native-tab-view';
 import type { MD3Theme } from 'react-native-paper';
+import type { MD3Type } from 'react-native-paper/lib/typescript/types';
 
 export type SceneMapRouteProps = Omit<SceneRendererProps, 'layout'> & { route: Route; };
 
@@ -16,6 +17,7 @@ export type WithRequiredFields<T, K extends keyof T> = T & Required<Pick<T, K>>;
 
 export interface AppTheme extends MD3Theme {
   // TODO: Refactor string => union with actual defined keys (primaryContainer, secondaryContainer, ...)
+  customFonts: Record<string, MD3Type>;
   styles: Record<string, ViewStyle | ImageStyle | TextStyle>;
   toggleScheme: () => void;
 }
