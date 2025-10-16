c79268f0da2d0294c1ee347d35147954576ebd83
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-10-17 00:27:20 +0300

    Frontend: Add utils/linking.ts

--- frontend/src/components/Link.tsx
+++ frontend/src/components/Link.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import * as Linking from 'expo-linking';
 import { Pressable, type PressableProps } from 'react-native';
 
 import StyledText, { type StyledTextProps } from './StyledText';
 
-import config from '../utils/config';
+import { openLink } from '../utils/linking';
 
 interface LinkProps extends Omit<PressableProps, 'children'> {
   children: StyledTextProps['children'];
@@ -16,18 +15,8 @@ interface LinkProps extends Omit<PressableProps, 'children'> {
 const Link = ({ children, email, href, ...props }: LinkProps) => {
   const url = email ? `mailto:${href}` : href;
 
-  const handlePress = () => {
-    if (!config.IS_MOBILE && !email) {
-      window.open(url);
-    } else {
-      Linking.openURL(url).catch((err: unknown) => {
-        console.error(`opening link "${url}"`, err);
-      });
-    }
-  };
-
   return (
-    <Pressable onPress={handlePress} {...props}>
+    <Pressable onPress={() => openLink(url)} {...props}>
       <StyledText variant="link">{children}</StyledText>
     </Pressable>
   );
