93095450f2d6ff8ac5494cb3c9dcaf0ec5bd2846
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-10-23 16:04:41 +0300

    Frontend: Refactor RN<->Web messaging (#64)
    * Frontend: Differentiate RN->leaflet and leaflet->RN messages
    
    * Frontend: Use new message types in leaflet bundle
    
    * Frontend: Refactor RN side message to leaflet type: RNLeafletMessage -> LeafletToRNMessage
    
    * Frontend: Delete RNLeafletMessage type, use new message types in RN
    
    * Frontend: Add typeguard for RNToLeafletMessage, parse all messages with typesafe parseJSON
    
    * Frontend: Refactor HtmlRenderer sendDataToWeb to accept string data

--- frontend/src/bundles/leaflet/msgBridgeToRN.ts
+++ frontend/src/bundles/leaflet/msgBridgeToRN.ts
@@ -1,28 +1,48 @@
 import type { WebViewMessageEvent } from 'react-native-webview';
 
 import type { GeoPos } from '../../types';
+import { isRNToLeafletMessage } from '../../utils/typeguards';
+import { parseJSON } from '../../utils/parsers';
 
-export type RNLeafletMessage =
-  | {
-      type: 'debug';
-      payload: {
-        echo?: string;
-        error?: string;
-        msg?: string;
-      };
-    }
-  | {
-      type: 'command';
-      payload: {
-        command: 'openUrl';
-        href: string;
-      } | {
-        command: 'setPosition';
-        position: GeoPos | null;
-      };
-    };
+export type RNLeafletBidirectionalMessages = {
+  debug: {
+    echo?: string;
+    error?: string;
+    msg?: string;
+  };
+};
 
-export const sendMsg = (data: RNLeafletMessage) => {
+export type LeafletToRNCommandMessage =
+  | { command: 'openUrl'; href: string; };
+
+export type RNToLeafletCommandMessage =
+  | { command: 'setPosition'; position: GeoPos | null; };
+
+type LeafletToRNMessageTypesPayloads = {
+  command: LeafletToRNCommandMessage;
+  debug: RNLeafletBidirectionalMessages['debug'];
+};
+
+export type LeafletToRNMessage = {
+  [T in keyof LeafletToRNMessageTypesPayloads]: {
+    type: T;
+    payload: LeafletToRNMessageTypesPayloads[T];
+  }
+}[keyof LeafletToRNMessageTypesPayloads];
+
+type RNToLeafletMessageTypesPayloads = {
+  command: RNToLeafletCommandMessage;
+  debug: RNLeafletBidirectionalMessages['debug'];
+};
+
+export type RNToLeafletMessage = {
+  [T in keyof RNToLeafletMessageTypesPayloads]: {
+    type: T;
+    payload: RNToLeafletMessageTypesPayloads[T];
+  }
+}[keyof RNToLeafletMessageTypesPayloads];
+
+const sendMsg = (data: LeafletToRNMessage) => {
   const msg = JSON.stringify(data);
   if (window.ReactNativeWebView) {
     window.ReactNativeWebView.postMessage(msg);
@@ -31,21 +51,24 @@ export const sendMsg = (data: RNLeafletMessage) => {
   }
 };
 
-const setOnMsgHandler = (handler: (msg: RNLeafletMessage) => void) => {
+const setOnMsgHandler = (handler: (msg: RNToLeafletMessage) => void) => {
   return (
     event: MessageEvent<string> | WebViewMessageEvent['nativeEvent'] // iframe contentWindow.postMessage | react-native-webview postMessage event
   ) => {
-    try {
-      handler(JSON.parse(event.data) as RNLeafletMessage);
-    } catch (err: unknown) {
+    const parsedData = parseJSON(isRNToLeafletMessage)(event.data);
+
+    if (parsedData.hasError) {
+      // NOTE: relay errors back to RN
       handler({
         type: 'debug',
         payload: {
-          error: JSON.stringify(err instanceof Error ? err.message : err),
-          echo: event.data,
+          error: parsedData.error, // Contains original event.data string
         },
       });
+      return;
     }
+
+    handler(parsedData.parsed);
   };
 };
 
